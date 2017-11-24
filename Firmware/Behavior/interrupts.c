15ab530064800d5c92a6b62f22db08fb35f47d29
Filipe Carvalho <34546039+filcarv@users.noreply.github.com>
2017-11-24 18:32:40 +0000

    Added hability to change the DOx frquency and Duty Cycle when already operating the DOx outputs.

--- Firmware/Behavior/interrupts.c
+++ Firmware/Behavior/interrupts.c
@@ -4,6 +4,8 @@
 #include "app_funcs.h"
 #include "hwbp_core.h"
 
+#include "structs.h"
+
 /************************************************************************/
 /* Declare application registers                                        */
 /************************************************************************/
@@ -74,3 +76,60 @@ ISR(PORTF_INT0_vect, ISR_NAKED)
 	reti();
 }
 
+/************************************************************************/
+/* PWM DOx                                                              */
+/************************************************************************/
+timer_conf_t timer_conf;
+is_new_timer_conf_t is_new_timer_conf;
+
+ISR(TCF0_OVF_vect, ISR_NAKED)
+{
+    if (is_new_timer_conf.pwm_do0)
+    {
+        TCF0_PER = timer_conf.target_do0;
+        TCF0_CCA = timer_conf.dcycle_do0;
+        TCF0_CTRLA = timer_conf.prescaler_do0;
+        is_new_timer_conf.pwm_do0 = false;
+    }
+    
+    reti();
+}
+
+ISR(TCE0_OVF_vect, ISR_NAKED)
+{
+    if (is_new_timer_conf.pwm_do1)
+    {
+        TCE0_PER = timer_conf.target_do1;
+        TCE0_CCA = timer_conf.dcycle_do1;
+        TCE0_CTRLA = timer_conf.prescaler_do1;
+        is_new_timer_conf.pwm_do1 = false;
+    }
+    
+    reti();
+}
+
+ISR(TCD0_OVF_vect, ISR_NAKED)
+{
+    if (is_new_timer_conf.pwm_do2)
+    {
+        TCD0_PER = timer_conf.target_do2;
+        TCD0_CCA = timer_conf.dcycle_do2;
+        TCD0_CTRLA = timer_conf.prescaler_do2;
+        is_new_timer_conf.pwm_do2 = false;
+    }
+    
+    reti();
+}
+
+ISR(TCC0_OVF_vect, ISR_NAKED)
+{
+    if (is_new_timer_conf.pwm_do3)
+    {
+        TCC0_PER = timer_conf.target_do3;
+        TCC0_CCA = timer_conf.dcycle_do3;
+        TCC0_CTRLA = timer_conf.prescaler_do3;
+        is_new_timer_conf.pwm_do3 = false;
+    }
+    
+    reti();
+}

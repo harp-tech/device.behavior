6a3d20f162e408c2cb719afadb48b14e420d5449
Filipe Carvalho <34546039+filcarv@users.noreply.github.com>
2022-01-22 15:54:11 +0000

    Increased firmware version

--- Firmware/Behavior/app.c
+++ Firmware/Behavior/app.c
@@ -32,7 +32,7 @@ void hwbp_app_initialize(void)
     uint8_t hwH = 1;
     uint8_t hwL = 2;
     uint8_t fwH = 2;
-    uint8_t fwL = 5;
+    uint8_t fwL = 6;
     uint8_t ass = 0;    
     
    	/* Start core */

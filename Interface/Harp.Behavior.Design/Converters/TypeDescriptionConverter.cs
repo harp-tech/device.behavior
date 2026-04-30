a62c4d35eb7f1a7e2b66109f375b6a049b76f1f3
glopesdev <g.lopes@neurogears.org>
2026-04-30 21:49:31 +0100

    Read tooltip text from description attribute

---
+++ Interface/Harp.Behavior.Design/Converters/TypeDescriptionConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.ComponentModel;
+using System.Globalization;
+using System.Reflection;
+using Avalonia.Data.Converters;
+
+namespace Harp.Behavior.Design.Converters;
+
+public class TypeDescriptionConverter : IValueConverter
+{
+    static readonly ConcurrentDictionary<Type, string> _cache = new();
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not Type t)
+            return value;
+        return _cache.GetOrAdd(t, static key =>
+            key.GetCustomAttribute<DescriptionAttribute>()?.Description ?? key.Name);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return null;
+    }
+}
